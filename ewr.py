52a8d5c09286246bd664446ceb2fd758ade17b63
Laizin v <107423612+laiz-in@users.noreply.github.com>
2022-09-23 12:23:05 +0400

    Delete ewr.py

--- ewr.py
+++
@@ -1,9 +0,0 @@
-@app.route('/scrap',methods=['POST','GET']) # route to show the review comments in a web UI
-@cross_origin()
-def index():
-    if request.method == 'POST':
-        try:
-            url = request.form['scrap'].replace(" ", "")
-            print(url)
-            video_url = VideoUrl.Video_page(url)
-            all_video_link_of_channel = video_url['video_url']
